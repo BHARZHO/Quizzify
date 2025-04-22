af9f0ad6b942f155cdc9d58e8c145728bd556c76
balogun14 <awwalbalogun06@gmail.com>
2025-04-22 09:47:09 +0100

    fix

--- Program.cs
+++ Program.cs
@@ -3,20 +3,10 @@
 
 // var builder = WebApplication.CreateBuilder(args);
 
-// // Get connection string
-// var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-// if (connectionString is null)
-// {
-//     throw new ArgumentNullException("PLese Provide a valid configuration");
-// }
-
-// // Register MySQL database context
-// builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySQL(connectionString));
 
 // builder.Services.AddControllersWithViews();
 
 // var app = builder.Build();
-// await app.Services.InitializeDbAsync();
 
 // app.UseHttpsRedirection();
 // app.UseStaticFiles();
@@ -30,16 +20,29 @@
 
 using Quiziffy.Data;
 using Microsoft.EntityFrameworkCore;
+using AspNetCoreHero.ToastNotification;
+using AspNetCoreHero.ToastNotification.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddNotyf(config=> { config.DurationInSeconds = 10;config.IsDismissable = true;config.Position = NotyfPosition.BottomRight; });
 
 // Configure MySQL with Entity Framework
-builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySQL("DefaultConnection"));
+// // Get connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+if (connectionString is null)
+{
+    throw new ArgumentNullException("PLese Provide a valid configuration");
+}
+
+// Register MySQL database context
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseMySQL(connectionString));
 
 var app = builder.Build();
+await app.Services.InitializeDbAsync(); // i added this for migrtions
+
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -47,6 +50,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
+app.UseNotyf();
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
